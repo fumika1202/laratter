6ad12504ca03ebaa71c758d38fd663aa761366dc
fumika1202 <s23m1013@bene.fit.ac.jp>
2024-09-24 20:15:51 +0900

    フォロー機能の実装

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\TweetLikeController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FollowController;
 
 Route::get('/', function () {
   return view('welcome');
@@ -18,7 +19,10 @@ Route::middleware('auth')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::post('/follow/{user}', [FollowController::class, 'store'])->name('follow.store');
+  Route::delete('/follow/{user}', [FollowController::class, 'destroy'])->name('follow.destroy'); 
   // 🔽 検索のルーティングを追加
+  Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
   Route::get('/tweets/search', [TweetController::class, 'search'])->name('tweets.search');
   Route::resource('tweets', TweetController::class);
   Route::post('/tweets/{tweet}/like', [TweetLikeController::class, 'store'])->name('tweets.like');
